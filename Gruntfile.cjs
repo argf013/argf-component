292a9e47ea87b0db9e548f2d74b52bd00bc6eb14
argf013 <alghi344@gmail.com>
2024-09-22 01:59:37 +0700

    init grunt

---
+++ Gruntfile.cjs
@@ -0,0 +1,23 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-bump');
+
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['bump']);
+};
