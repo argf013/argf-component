64014d4ebc4dac0120ce05ea792dac24ac0ea7b2
argf013 <alghi344@gmail.com>
2024-09-21 22:28:07 +0700

    chore: update module builder configuration

---
+++ index.ts
@@ -0,0 +1,7 @@
+import Button from './src/components/Button/Button';
+import Dialog from './src/components/Dialog/Dialog';
+import DialogConfirm from './src/components/DialogConfirm/DialogConfirm';
+import Toast from './src/components/Toast/Toast';
+import useToast from './src/components/Toast/useToast';
+import { ToastProvider } from './src/components/Toast/ToastProvider';
+export { Button, Toast, ToastProvider, useToast, Dialog, DialogConfirm };
