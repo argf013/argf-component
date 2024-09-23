6527a8c5f1c6a5b880f331535e528e677c47c0ae
argf013 <alghi344@gmail.com>
2024-09-23 21:24:56 +0700

    feat: add dropdown

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,159 @@
+import { ChevronDownIcon } from '@primer/octicons-react';
+import React, { useState, useEffect, useRef, CSSProperties } from 'react';
+import clsx from 'clsx';
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface DropdownProps {
+  options: Option[];
+  onSelect: (option: Option) => void;
+  placeholder?: string;
+  size?: 'small' | 'medium' | 'large';
+  disabled?: boolean;
+  initialValue?: Option;
+  style?: CSSProperties;
+  className?: string;
+  labelClassName?: string;
+  isSearchable?: boolean;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({
+  options,
+  onSelect,
+  placeholder = 'Select an option',
+  size = 'medium',
+  disabled = false,
+  initialValue = null,
+  style,
+  className,
+  labelClassName,
+  isSearchable = false,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<Option | null>(
+    initialValue,
+  );
+  const [searchTerm, setSearchTerm] = useState('');
+  const [maxHeight, setMaxHeight] = useState('0px');
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (initialValue) {
+      setSelectedOption(initialValue);
+    }
+  }, [initialValue]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+        setMaxHeight('0px');
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleToggle = () => {
+    if (!disabled) {
+      setIsOpen(!isOpen);
+      const newMaxHeight = isOpen
+        ? '0px'
+        : `${Math.min(options.length * 180)}px`;
+      setMaxHeight(newMaxHeight);
+    }
+  };
+
+  const handleSelect = (option: Option) => {
+    if (!disabled) {
+      setSelectedOption(option);
+      onSelect(option);
+      setIsOpen(false);
+      setSearchTerm('');
+      setMaxHeight('0px');
+    }
+  };
+
+  const filteredOptions = options.filter((option) =>
+    option.label.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  const sizeClass =
+    size === 'small' ? 'w-32' : size === 'large' ? 'w-64' : 'w-48';
+
+  return (
+    <div
+      ref={dropdownRef}
+      className={clsx('relative inline-block text-left', sizeClass, className)}
+      style={style}
+    >
+      <button
+        onClick={handleToggle}
+        className={clsx(
+          'inline-flex items-center gap-2 justify-left',
+          sizeClass,
+          'rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50',
+          { 'cursor-not-allowed opacity-50': disabled },
+        )}
+        disabled={disabled}
+      >
+        <span
+          className={clsx({ 'text-gray-400': !selectedOption }, labelClassName)}
+        >
+          {selectedOption ? selectedOption.label : placeholder}
+        </span>
+        <ChevronDownIcon className='ml-auto' />
+      </button>
+      <div
+        className={clsx(
+          'absolute z-50 mt-2 transition-all duration-300 ease-in-out',
+          sizeClass,
+          'rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5',
+          {
+            'opacity-0': !isOpen,
+            'opacity-100': isOpen,
+            invisible: !isOpen,
+            visible: isOpen,
+          },
+        )}
+        style={{
+          maxHeight: maxHeight,
+          transition:
+            'max-height 0.3s ease, opacity 0.3s ease, visibility 0.3s ease',
+        }}
+      >
+        {isSearchable && (
+          <input
+            type='text'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className='w-full px-4 font-medium py-2 text-sm rounded-t-md border-b border-gray-300 focus:outline-none'
+            placeholder='Search...'
+          />
+        )}
+        <ul className='max-h-60 overflow-auto px-2 py-2'>
+          {filteredOptions.map((option, index) => (
+            <li
+              key={index}
+              onClick={() => handleSelect(option)}
+              className='cursor-pointer select-none py-2 px-3 font-medium text-sm relative rounded-md hover:bg-gray-100 hover:text-dark'
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/playground/Playground.tsx
+++ src/playground/Playground.tsx
@@ -1,109 +1,18 @@
-import React, { useState } from 'react';
-import { Accordion, AccordionItem } from '../components/Accordion/Accordion';
-import Menu, { MenuItem } from '../components/Menu/Menu';
-import { MailIcon, PersonIcon, TrashIcon } from '@primer/octicons-react';
-import Dialog from '../components/Dialog/Dialog';
-import DialogConfirm from '../components/DialogConfirm/DialogConfirm';
-import useToast from '../components/Toast/useToast';
+import React from 'react';
+import Dropdown from '../components/Dropdown/Dropdown';
 
 const Playground = () => {
-  const [dialogVisible, setDialogVisible] = useState(false);
-  const [dialogConfirmVisible, setDialogConfirmVisible] = useState(false);
-  const { addToast } = useToast();
-  const items: AccordionItem[] = [
-    {
-      title: 'What is Lorem Ipsum?',
-      content: (
-        <>
-          <p className='mb-2 text-gray-500'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s.
-          </p>
-        </>
-      ),
-    },
-    {
-      title: 'Why do we use it?',
-      content: (
-        <>
-          <p className='mb-2 text-gray-500 dark:text-gray-400'>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters.
-          </p>
-        </>
-      ),
-    },
-    {
-      title: 'Where can I get some?',
-      content: (
-        <>
-          <p className='mb-2 text-gray-500 dark:text-gray-400'>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don&apos;t look even slightly
-            believable.
-          </p>
-        </>
-      ),
-    },
-  ];
-
-  const menuItems: MenuItem[] = [
-    {
-      label: 'Show Dialog',
-      icon: <TrashIcon />,
-      onClick: () => setDialogVisible(true),
-    },
-    {
-      label: 'Show Confirm Dialog',
-      icon: <PersonIcon />,
-      onClick: () => setDialogConfirmVisible(true),
-    },
-    {
-      label: 'Logout',
-      icon: <MailIcon />,
-      danger: true,
-      onClick: () => {
-        addToast({
-          message: 'Logged out successfully',
-          severity: 'success',
-        });
-      },
-    },
-  ];
-
   return (
     <div className='max-w-2xl flex justify-center flex-col mx-auto'>
       <h1>Playground</h1>
-      <Menu items={menuItems} className='my-4' />
-      <Accordion items={items} multiple={true} animationDuration={300} />
-      <Menu items={menuItems} className='my-4' vertical />
-
-      <Dialog
-        header='Dialog Header'
-        visible={dialogVisible}
-        onClose={() => setDialogVisible(false)}
-        closeable
-        size='medium'
-        severity='info'
-      >
-        <p>Dialog content goes here</p>
-      </Dialog>
-
-      <DialogConfirm
-        header='Confirm Dialog Header'
-        visible={dialogConfirmVisible}
-        severity='danger'
-        message='Are you sure you want to delete this item?'
-        items={['This action cannot be undone']}
-        onCancel={() => setDialogConfirmVisible(false)}
-        onSubmit={() => {
-          alert('Confirmed');
-          setDialogConfirmVisible(false);
-        }}
+      <Dropdown
+        placeholder='Select an A'
+        options={[
+          { label: 'Option 1', value: 'option1' },
+          { label: 'Option 2', value: 'option2' },
+        ]}
+        onSelect={(option) => alert(option.label)}
+        isSearchable
       />
     </div>
   );
